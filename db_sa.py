515fa2c325554dca7801f4ef865b2d00d9d9b548
Amar <rautamar705@gmail.com>
2023-02-03 10:55:00 +0530

    Decryption for single word is executed

--- db_sa.py
+++ db_sa.py
@@ -2,7 +2,7 @@ from random import choice
 alpha_bets = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 def encodingDB(mes):
     mes = mes.lower()
-    if(len(mes)<=2):
+    if(len(mes)<3):
         return mes[::-1]
     elif(len(mes)>=3):
         new_str = ""
@@ -18,3 +18,18 @@ def encodingDB(mes):
         return new_str
 # print(encodingDB("Dragon"))
 
+def decryptionDB(mes):
+    if(len(mes)<3):
+        res =  mes[::-1]
+        res = res.capitalize()
+    elif(len(mes)>=3):
+        new_str = ""
+        new_str += mes[3:-3]
+        res = ""
+        res += new_str[-1]
+        res += new_str[:-1]
+        res = res.capitalize()
+        return res
+check = encodingDB("Dragon")
+print(check)
+print(decryptionDB(check))
